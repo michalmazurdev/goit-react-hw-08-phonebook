f25fea14a1dffebb15694caf6f37e66ed8a5a24d
michalmazurdev <m.mazur40@gmail.com>
2023-07-30 20:56:40 +0200

    home page update

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import css from './Home.module.css';
 
 export const Home = () => {
@@ -5,8 +6,12 @@ export const Home = () => {
     <div className={css.homeContainer}>
       <h1 className={css.heading}>Welcome to Phonebook application</h1>
       <p className={css.text}>
-        Please register in order to use the funcionality of the app where you
-        can store all your phone contacts!
+        Please{' '}
+        <Link className={css.link} to="/register">
+          register
+        </Link>{' '}
+        in order to use the funcionality of the app where you can store all your
+        phone contacts!
       </p>
     </div>
   );
